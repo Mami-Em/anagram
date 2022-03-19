0c3e5943683e81fbfdf3cd0c52232fff510bbf9e
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-18 23:45:29 -0400

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -20,7 +20,7 @@ int main() {
 int c_f_anagram(string a, string b) {
     for (int i = 0; i < strlen(a); i++)
     {
-        // funtion to correct
+        // funtion to complete
         printf("%c\n", a[i]);
     }
     return 0;
