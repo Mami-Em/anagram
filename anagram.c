cb25385945047ff7bf63dc3872a98f683bdf30f0
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-19 01:14:30 -0400

    Update anagram.c
    "version 1.0.0"

--- anagram.c
+++ anagram.c
@@ -1,37 +1,44 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int anagram(string a,string b);
 
 int main() {
     string a = get_string("Insert the word: ");
     string b = get_string("Compare with: ");
-    if (strlen(a) == strlen(b))
-    {
-        /* code */
-        anagram(a,b);
-    } else {
-        printf("They do not have the same size");
-    }
-    return 0;
+    anagram(a,b);
 }
 
 int anagram(string a, string b) {
-    char val[strlen(a)];
-    int t, f;
-    for (int i = 0; i < strlen(a); i++)
-    {
-        for (int j = 0; j < strlen(a); j++)
+    char val;
+    string new;
+
+    for (int i = 0; i < strlen(a); i++) {
+        // pick one by one each value in A
+        val = a[i];
+        for (int j = 0; j < strlen(b); j++)
         {
-            if (a[i] == b[j])
-                t ++;
-            else
-                f ++;
-        }
-        
+            // check if each word in A appear in B
+            if (val == b[j]) {
+                new[i] = val;
+            }
+        } 
     }
-    printf("%i true\n%i fale\n", t,f);
-    return 0;
+
+    // if new str and first str are same
+    if (strcmp(new, a) == 0)
+        printf("They are anagrams\n ");
+
+    // if there are similarities
+    else if (strlen(new) > 1)
+        /* -1 bc there is already one value by default inside of the new str */
+        printf("There are only %lu words that matches!\n", strlen(new)-1);
     
+    // not anagrams
+    else
+        printf("They are not anagrams!\n");
+    
+    return 0;
 }
