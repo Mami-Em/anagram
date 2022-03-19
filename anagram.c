96e5ee200efefd137b233140b2428b31d1f2a486
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-18 23:44:25 -0400

    Add files via upload

---
+++ anagram.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int c_f_anagram(string a,string b);
+
+int main() {
+    string a = get_string("Insert the first word: ");
+    string b = get_string("Insert the word to comparee it with: ");
+    if (strlen(a) == strlen(b))
+    {
+        /* code */
+        c_f_anagram(a,b);
+    } else {
+        printf("They do not have the same size");
+    }
+    return 0;
+}
+
+int c_f_anagram(string a, string b) {
+    for (int i = 0; i < strlen(a); i++)
+    {
+        printf("%c\n", a[i]);
+    }
+    return 0;
+    
+}
