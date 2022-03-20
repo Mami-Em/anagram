28c496b35f22cb621ad0a8dd27afefee6e9bcb95
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-19 20:36:18 -0400

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,12 @@
+from cs50 import get_string
+
+def cut(*ipt, int):
+    for i in range(x):
+        print(ipt[i])
+
+def main():
+    ipt = get_string("user prompt: ")
+    x = len(ipt)
+    cut(ipt, x)
+
+main()
