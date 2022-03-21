d0e021e44f446041c39be0435eadd0828548da5e
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-20 20:57:23 -0400

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -5,10 +5,37 @@
 
 int anagram(string a,string b);
 
+char *shorting2(char *val) {
+    char to_check;
+    char *p = val;
+    int count = 0;
+    // if char not inside the new array yet, insert it
+    for (int i = 0; i < strlen(val); i++)
+    {
+        if(val[i] == ' ') continue;
+
+        to_check = val[i];
+        /* code */
+        for (int j = i; j < strlen(val); j++)
+        {
+
+            /* code */
+            if (to_check == val[j]){
+                count ++;
+                val[j] = ' ';}
+        }
+        
+        count = 0;
+    }
+    printf("%s \n", val);
+    return val;
+}
+
 int main() {
     string a = get_string("Insert the word: ");
     string b = get_string("Compare with: ");
-    anagram(a,b);
+    
+    anagram(shorting2(a),shorting2(b));
 }
 
 int anagram(string a, string b) {
@@ -29,6 +56,7 @@ int anagram(string a, string b) {
                 same ++;
         } 
     }
+    
     
     // if new str and first str are same
     if (same == strlen(a))
